b3006119fc437898e1454cec4b9400528098fb82
Steven Pugia <spugia@purdue.edu>
2019-09-01 16:54:04 -0400

    update

--- air_breathing/ideal_ramscramjet.m
+++ air_breathing/ideal_ramscramjet.m
@@ -1,7 +1,7 @@
 %.. ideal ram or scram jet
 %.. english units only
 
-function [Isp, cont] = ideal_ramscramjet(V0, M, P0, T0, CPR, T05, T07, Hc, Cp, k)
+function [Isp, cont] = ideal_ramscramjet(V0, M, P0, T0, T04, Hc, Cp, k)
 
   btuconv = 778.169;
   g = 32.17405;
@@ -11,22 +11,22 @@ function [Isp, cont] = ideal_ramscramjet(V0, M, P0, T0, CPR, T05, T07, Hc, Cp, k
   T00 = T0 * (1 + (k - 1) / 2 * M ^ 2);
 
   %.. inlet
-  P02 = P00 * ideal_inlet(M);
-  T02 = T00;
+  P03 = P00 * ideal_inlet(M);
+  T03 = T00;
 
   %.. combustor
-  P05 = P03;
-  f = Cp*(T05 - T03) / (Hc - Cp*T05);
+  P04 = P03;
+  f = Cp*(T04 - T03) / (Hc - Cp*T04);
 
   %.. nozzles
-  V9 = sqrt(2*Cp*btuconv*g*T07*(1 - (P0 / P07)^((k-1) / k)));
+  V9 = sqrt(2*Cp*btuconv*g*T04*(1 - (P0 / P04)^((k-1) / k)));
 
   %.. performance characteristics
-  Isp = ((1 + f + fab)*V9 - V0) / ((f + fab) * g);
+  Isp = ((1 + f)*V9 - V0) / (f * g);
 
   cont = false;
 
-  if f > 0 & fab > 0 & Isp > 0
+  if f > 0 & Isp > 0
     cont = true;
   end
 end
