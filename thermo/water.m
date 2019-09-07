d3314c565846a29af9dbae0dd1ad709708741bb0
Steven Pugia <spugia@purdue.edu>
2019-09-07 10:37:22 -0400

    adding file for water properties

---
+++ thermo/water.m
@@ -0,0 +1,10 @@
+%.. computes thermodynamic properties of water
+%.. valid for T = 60 -> 1900 K
+
+% T = temperature of water
+% Pv = [], T = [K], Cp/Cv/R = [J/kg-k],   k = [], Mw = [kg/mol]
+
+function [Pv, Cp, Cv, k, R, Mw] = water(T)
+
+	% TODO
+end
