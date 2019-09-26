da2a27a6ec7c495ef7bcc2d25a1ebe7850f8e31c
Steven Pugia <spugia@purdue.edu>
2019-09-26 14:28:54 -0400

    variable name change

--- mixing/cpm_upt.m
+++ mixing/cpm_upt.m
@@ -1,7 +1,7 @@
 %.. computes the mixed property of two streams
 %.. applicable for velocity, stagnation temperature, and stagnation pressure
 
-function [Pm] = cpm_upt(Pp, Ps, a)
+function [m] = cpm_upt(p, s, a)
 
-	Pm = (Pp + a*Ps) / (1 + a);
+	m = (p + a*s) / (1 + a);
 end
