40f0053e58ff87c579cbdee3ea9394e441a69df4
Steven Pugia <spugia@purdue.edu>
2019-10-09 14:51:27 -0400

    updating real turbojet with bypass

--- systems/turbojet_bp_ab.m
+++ systems/turbojet_bp_ab.m
@@ -1,7 +1,7 @@
-%.. ideal turbojet performance with bypass and afterburner
+%.. real turbojet performance with bypass and afterburner
 %.. english units only
 
-function [Isp] = ideal_turbojet(M0, Hc, Cp, k, CPR, T04, T07, etas)
+function [Fps, SFC, f, Tts, Pts] = ideal_turbojet(u0, Hc, Cp, k, Beta, CPR, T04, T07, etas)
 
   %.. constants
   btuconv = 778.169;
@@ -19,6 +19,40 @@ function [Isp] = ideal_turbojet(M0, Hc, Cp, k, CPR, T04, T07, etas)
   T02 = T00;
 
   %.. compressor
-  
+  T03 = T02*(1+1/nc*(CPR^((k-1)/k)-1))^(k/(k-1));
+  P03 = CPR * P02;
+
+  %.. burber
+  fb = (Cp*(T04-T03)) / (nb*Hc);
+
+  P04 = P03;
+
+  %.. turbine
+  T05 = T04 - (1+fb)*(T03-T02) / nt;
+  P05 = P04*(T05/T04)^((k-1)/k);
+
+  %.. constant pressure mixer
+  T06 = ((1+fb)*T05 + Beta*T02) / (1+fb+Beta);
+  P06 = ((1+fb)*P05 + Beta*P02) / (1+fb+Beta);
+
+  %.. afterburner
+  fab = (Cp*(1+Beta+fb)*(T07-T06)) / (nab*Hc);
+
+  P07 = P06;
+
+  %.. nozzle
+  T09 = T07;
+  P09 = P07;
+
+  v9 = sqrt(2*Cp*btuconv*g*nn*T05*(1-(P9/P07)^((k-1)/k)))
+
+  %.. performance parameters
+  Fsp = ((1+Beta+fb+fab)*v9 - (1+Beta)*v0) / (g*(1+Beta)); 
+  SFC = (g*(fb+fab)) / ((1+Beta+fb+fab)*v9 - (1+Beta)*u0);
+
+  %.. compressing data
+  f   = [fb, fab];
+  Tts = [T00, T02, T03, T04, T05, T06, T07, T09];
+  Pts = [P00, P02, P03, P04, P05, P06, P07, P08];
 
 end
